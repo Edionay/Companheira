9b35ab9864cc692912107e5b13c393f382eaf894
Edionay <edionay@gmail.com>
2017-05-30 02:44:19 -0300

    Teste de inclusão de novo discente.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,28 @@ function botaoExcluirClicado(idDaLinha) {
         tabelaDeTarefas.deleteRow(linhaASerApagada.rowIndex);
     }
 }
+
+function incluirDiscente() {
+    // Get a reference to the table
+    let tabelaDeDiscentes = document.getElementById("tabela-de-discentes");
+    let nome = document.getElementById("nome_do_discente").innerText;
+    let matricula = document.getElementById("matricula");
+    let curso = document.getElementById("curso");
+    let email = document.getElementById("email");
+    let telefone1 = document.getElementById("telefone_1");
+    let telefone2 = document.getElementById("telefone_2");
+
+    // Insert a row in the table at row index 0
+    let novoDiscente = tabelaDeDiscentes.insertRow(1);
+
+    // Insert a cell in the row at index 0
+    let campo1 = novoDiscente.insertCell(0);
+
+
+    // Append a text node to the cell
+    let teste = document.createTextNode(nome);
+    campo1.appendChild(teste);
+
+
+}
+
